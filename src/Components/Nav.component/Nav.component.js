e9d3f341a74c41160e07a442e57d2bc80cef9229
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-06-08 17:27:06 -0600

    more updates to nav bar

--- src/Components/Nav.component/Nav.component.js
+++ src/Components/Nav.component/Nav.component.js
@@ -1,6 +1,8 @@
 import Logo from '../../Logo/Logo';
 import './Nav.component.css';
 import { IoIosArrowDown } from 'react-icons/io'
+import { LuSearch } from 'react-icons/lu';
+import { BsTelephone } from 'react-icons/bs';
 
 const Nav = () => {
   return (
@@ -12,8 +14,16 @@ const Nav = () => {
           <h4 className='link'>Cameras <IoIosArrowDown className='down-arrow' /></h4>
           <h4 className='link'>Smart Home <IoIosArrowDown className='down-arrow' /></h4>
           <h4 className='link'>Services <IoIosArrowDown className='down-arrow' /></h4>
-          <h4 className='link'>How To Buy <IoIosArrowDown className='down-arrow' /></h4>
+          <h4 className='link'>How To Buy</h4>
         </nav>
+      </div>
+      <div className='login-search-container'>
+        <h5 className='login'>Login</h5>
+        <LuSearch className='search-icon' />
+      </div>
+      <div className='phone-button-container'>
+        <h5 className='peace-of-mind'>GET PEACE OF MIND TODAY</h5>
+        <button className='phone-button'><BsTelephone /> 801.992.1424</button>
       </div>
     </div>
   )
