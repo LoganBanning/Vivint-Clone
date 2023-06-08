2b3d12a10c2504fa547fcdfca80a519af590216e
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-06-08 16:20:29 -0600

    updated nav and nav links

--- src/Components/Nav.component/Nav.component.js
+++ src/Components/Nav.component/Nav.component.js
@@ -1,10 +1,20 @@
 import Logo from '../../Logo/Logo';
 import './Nav.component.css';
+import { IoIosArrowDown } from 'react-icons/io'
 
 const Nav = () => {
   return (
     <div className='nav-container'>
       <Logo />
+      <div className='nav-links-container'>
+        <nav className='nav-links'>
+          <h4 className='link'>Home Security <IoIosArrowDown className='down-arrow' /></h4>
+          <h4 className='link'>Cameras <IoIosArrowDown className='down-arrow' /></h4>
+          <h4 className='link'>Smart Home <IoIosArrowDown className='down-arrow' /></h4>
+          <h4 className='link'>Services <IoIosArrowDown className='down-arrow' /></h4>
+          <h4 className='link'>How To Buy <IoIosArrowDown className='down-arrow' /></h4>
+        </nav>
+      </div>
     </div>
   )
 }
