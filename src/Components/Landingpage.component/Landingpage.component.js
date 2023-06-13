cb3d25aaaf800251b5004064b8235c3379c69cfa
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-06-13 17:40:03 -0600

    updated photo and photo text on landing page

--- src/Components/Landingpage.component/Landingpage.component.js
+++ src/Components/Landingpage.component/Landingpage.component.js
@@ -1,9 +1,23 @@
 import Nav from '../Nav.component/Nav.component';
+import SecurityPhoto from '../../Images/Vivint-security-products.jpg';
+import './Landingpage.component.css';
+import { BsTelephone } from 'react-icons/bs';
 
 const Landingpage = () => {
   return (
-    <div>
+    <div className='landing-page-container'>
     <Nav />
+    <div className='top-landing-page'>
+    </div>
+    <img src={SecurityPhoto} alt='Security Equipment' className='equipment-photo' />
+    <div className='photo-text'>
+      <p className='text-one'>SUPERIOR HOME TECHNOLOGY</p>
+      <h1 className='text-two'>A complete smart home<br /> system</h1>
+      <p className='text-three'><span className='beginning-text'>Make home control easier while increasing </span>the convenience,<br /> security, and efficiency of your living space.</p>
+      <p className='text-four'>CUSTOMIZE WITH A SECURITY EXPERT</p>
+      <button className='phone-button-photo'><BsTelephone/> 801.992.1424</button>
+    </div> 
+
     </div>
   )
 }
