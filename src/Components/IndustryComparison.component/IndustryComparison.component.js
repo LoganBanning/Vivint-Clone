3bfa9aa7f01b21f9f2fed6f7f93cd1cc2aac2fc0
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-06-14 16:59:08 -0600

    removed alert

---
+++ src/Components/IndustryComparison.component/IndustryComparison.component.js
@@ -0,0 +1,15 @@
+import './IndustryComparison.component.css';
+
+const IndustryComparison = () => {
+  return (
+    <div className='industry-comparison-comp-container'>
+      <div className='start-text-IC-comp'>
+        <h4 className='text-IC-1'>Home automation companies</h4>
+        <p className='text-IC-2'>Vivint leads the industry in every single category.</p>
+        <p className='text-IC-3'>(last updated April 2023)</p>
+      </div>
+    </div>
+  );
+}
+
+export default IndustryComparison;
