6e69ae316c21644054dddd6558f32bdd62a4f11c
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-06-16 17:11:16 -0600

    set up footer, added functionality to inputs on the quote input component

--- src/Components/Footer.component/Footer.component.js
+++ src/Components/Footer.component/Footer.component.js
@@ -1,8 +1,24 @@
 import './Footer.component.css';
+import VivintHouseLogo from '../../Images/vivint-house-logo.png';
 
 const Footer = () => {
   return (
-    <div></div>
+    <div className='footer-comp-container'>
+      <div className='footer-comp-main-links'>
+        <img src={VivintHouseLogo} alt='Vivint Logo' className='vivint-house-logo-footer' />
+        <p className='footer-comp-link'>Customer Support</p>
+        <p className='footer-comp-link'>Licenses</p>
+        <p className='footer-comp-link'>Privacy Policy</p>
+        <p className='footer-comp-link'>Term of Service</p>
+      </div>
+      <div className='footer-comp-copyright-opt-out-links'>
+        <p>Copyright 2023 Vivint, Inc.  All rights reserved.</p>
+        <div className='copyright-links-footer'>
+          <p className='copyright-link'>Do not sell or share my personal infromation.</p>
+          <p className='copyright-link'>Limit the use of my sensitive personal information.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/Components/QuoteInput.component/QuoteInput.component.js
+++ src/Components/QuoteInput.component/QuoteInput.component.js
@@ -1,6 +1,15 @@
 import './QuoteInput.component.css';
+import { useState } from 'react';
 
 const QuoteInputComponent = () => {
+
+  const [ name, setName ] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  console.log(name);
+  console.log(phoneNumber);
+  console.log(email);
+
   return (
     <div className='quote-input-comp-container'>
       <div className='quote-input-comp-text'>
@@ -9,9 +18,21 @@ const QuoteInputComponent = () => {
       </div>
       <div className='quote-input-comp-form-container'>
       <form className='quote-input-comp-form'>
-        <input className='quote-input-comp-input' placeholder='First and Last name' ></input>
-        <input className='quote-input-comp-input' placeholder='Phone' ></input>
-        <input className='quote-input-comp-input' placeholder='Email' ></input>
+        <input 
+        className='quote-input-comp-input' 
+        placeholder='First and Last name'
+        onChange={(e) => setName(e.target.value)} >
+        </input>
+        <input 
+        className='quote-input-comp-input' 
+        placeholder='Phone'
+        onChange={(e) => setPhoneNumber(e.target.value)} >
+        </input>
+        <input 
+        className='quote-input-comp-input' 
+        placeholder='Email'
+        onChange={(e) => setEmail(e.target.value)} >
+        </input>
       </form>
       <p className='quote-input-comp-disclaimer'>Clicking the button below constitutes your electronic signature and consent for Vivint<br /> and its parent company NRG to use automated technology, including calls, texts and<br /> prerecorded messages, to contact you at the number provided about Vivint and NRG<br /> offers. Consent not required to make a purchase. Up to 10msg/month. Reply 'STOP' to <br />opt-out. See Terms of Service and Privacy Policy.</p>
       <button className='orange-button'>Start Your Quote</button>
