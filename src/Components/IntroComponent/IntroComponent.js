0b4d1d20aab500f071a02e68ae1d0a596fdce5ef
LoganBanning <loganboganb@gmail.com>
2025-08-27 21:45:04 -0600

    styling updates

---
+++ src/Components/IntroComponent/IntroComponent.js
@@ -0,0 +1,21 @@
+import { BsTelephone } from 'react-icons/bs';
+import SecurityPhoto from '../../Images/Vivint-security-products.jpg';
+import './IntroComponent.css';
+
+
+const IntroComponent = () => {
+    return (
+        <div className='top-landing-page'>
+            <div className='photo-text'>
+                <p className='text-one'>SUPERIOR HOME TECHNOLOGY</p>
+                <h1 className='text-two'>A complete smart home<br /> system</h1>
+                <p className='text-three'><span className='beginning-text'>Make home control easier while increasing </span>the convenience,<br /> security, and efficiency of your living space.</p>
+                <p className='text-four'>CUSTOMIZE WITH A SECURITY EXPERT</p>
+                <button className='phone-button-photo'><BsTelephone/> 801.992.1424</button>
+            </div> 
+            <img src={SecurityPhoto} alt='Security Equipment' className='equipment-photo' />
+        </div>
+    )
+}
+
+export default IntroComponent;
--- src/Components/Landingpage.component/Landingpage.component.js
+++ src/Components/Landingpage.component/Landingpage.component.js
@@ -1,7 +1,6 @@
 import Nav from '../Nav.component/Nav.component';
-import SecurityPhoto from '../../Images/Vivint-security-products.jpg';
 import './Landingpage.component.css';
-import { BsTelephone } from 'react-icons/bs';
+import IntroComponent from '../IntroComponent/IntroComponent';
 import FeaturesComponent from '../Features.component/Features.component';
 import AppFeatures from '../App.Features.component/App.Features.component';
 import SmartDevicesComponent from '../SmartDevices.component/SmartDevices.component';
@@ -11,17 +10,8 @@ import Footer from '../Footer.component/Footer.component';
 const Landingpage = () => {
   return (
     <div className='landing-page-container'>
-    <Nav />
-    <div className='top-landing-page'>
-    </div>
-    <img src={SecurityPhoto} alt='Security Equipment' className='equipment-photo' />
-    <div className='photo-text'>
-      <p className='text-one'>SUPERIOR HOME TECHNOLOGY</p>
-      <h1 className='text-two'>A complete smart home<br /> system</h1>
-      <p className='text-three'><span className='beginning-text'>Make home control easier while increasing </span>the convenience,<br /> security, and efficiency of your living space.</p>
-      <p className='text-four'>CUSTOMIZE WITH A SECURITY EXPERT</p>
-      <button className='phone-button-photo'><BsTelephone/> 801.992.1424</button>
-    </div> 
+      <Nav />
+      <IntroComponent />
       <FeaturesComponent />
       <AppFeatures />
       <SmartDevicesComponent />
