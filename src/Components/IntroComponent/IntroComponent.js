54a48b06b834d2713c4888d2c051f094f546961b
LoganBanning <loganboganb@gmail.com>
2025-09-15 19:54:14 -0600

    adjusted the IntroComponent

--- src/Components/IntroComponent/IntroComponent.js
+++ src/Components/IntroComponent/IntroComponent.js
@@ -9,11 +9,11 @@ const IntroComponent = () => {
             <div className='photo-text'>
                 <p className='text-one'>SUPERIOR HOME TECHNOLOGY</p>
                 <h1 className='text-two'>A complete smart home<br /> system</h1>
+                <img src={SecurityPhoto} alt='Security Equipment' className='equipment-photo' />
                 <p className='text-three'><span className='beginning-text'>Make home control easier while increasing </span>the convenience,<br /> security, and efficiency of your living space.</p>
                 <p className='text-four'>CUSTOMIZE WITH A SECURITY EXPERT</p>
                 <button className='phone-button-photo'><BsTelephone/> 801.992.1424</button>
             </div> 
-            <img src={SecurityPhoto} alt='Security Equipment' className='equipment-photo' />
         </div>
     )
 }
